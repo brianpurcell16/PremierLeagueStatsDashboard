631b43958b1020fcd356f49b9f23554bd0ee8b37
brianpurcell16 <brian.purcell16@gmail.com>
2026-06-11 15:47:09 +0100

    created initial frontend and backend
    Using react through node and the spring initializer i created the initial project structure with react as the frontend and spring boot as the backend

---
+++ backend/football-api/src/test/java/com/footballdash/football_api/FootballApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballdash.football_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
